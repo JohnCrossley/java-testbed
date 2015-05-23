94c5cb154f4616dee32f00dd90a683c5f4ac5094
johncrossley <nhojcc>
2015-05-23 17:41:38 +0100

    Initial commit

---
+++ src/com/jccworld/javatestbed/Main.java
@@ -0,0 +1,11 @@
+package com.jccworld.javatestbed;
+
+/**
+ * User: jcc
+ * Date: 23/05/15
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[JCC] Main.main() -> hello world!");
+    }
+}
